7b9b8a7e563a3afc8b72b0e3584c9d148e9175b7
emalderete <nicolas.alderete95@live.com>
2022-03-23 18:20:57 -0300

    Maquetada página de registro "Register.js"

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Navbar from './common/Navbar';
+import './common/Components.css';
 
 const Register = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <h1 className='loginTitle'>Registrarme</h1>
+            <form className='loginForm'>
+                <div className="loginContainer">
+                    <label className='loginLabel' name="userName">Usuario</label>
+                    <input className='loginInput' type="text" name="userName"></input>
+                </div>
+                <div className="loginContainer">
+                    <label className='loginLabel' name="password">Contraseña</label>
+                    <input className='loginInput' type="password" name="password"></input>
+                </div>
+                <button className='loginSubmit' type='submit'>Registrar</button>
+            </form>
         </div>
     );
 };
